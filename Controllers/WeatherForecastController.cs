f619f66b5f8572a9d08f4e92a9b06912c09fb5a5
Antariksh <er.antariksh19@gmail.com>
2022-01-26 01:33:54 +0530

    CRUD operations in Dynamo DB

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DynamoDbDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IDynamoDBContext _dbContext;
+
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDynamoDBContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="City"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get(string City = "Noida")
+        {
+            return await _dbContext
+                        .QueryAsync<WeatherForecast>(City)
+                        .GetRemainingAsync();
+            // return GenerateDummyWeatherForcast(City);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="City"></param>
+        /// <returns></returns>
+
+        [HttpPost]
+        public async Task PostData(string City)
+        {
+            var Forcasts = GenerateDummyWeatherForcast(City);
+            foreach (var item in Forcasts)
+            {
+                await _dbContext.SaveAsync(item);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task UpdateData(string city)
+        {
+            var getSpecificData =
+                 await _dbContext.LoadAsync<WeatherForecast>(city, DateTime.UtcNow.Date.AddDays(1));
+            getSpecificData.Summary = "Chilled";
+            await _dbContext.SaveAsync(getSpecificData);
+        }
+
+        [HttpDelete]
+        public async Task DeleteData(string city)
+        {
+            var getSpecificData =
+                 await _dbContext.LoadAsync<WeatherForecast>(city, DateTime.UtcNow.Date.AddDays(1));
+            await _dbContext.DeleteAsync(getSpecificData);
+        }
+
+
+        private IEnumerable<WeatherForecast> GenerateDummyWeatherForcast(string city)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                City = city,
+                Date = DateTime.UtcNow.Date.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace DynamoDbDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DynamoDbDemo", Version = "v1" });
+            });
+
+            var credentials = new Amazon.Runtime.BasicAWSCredentials("YOUR KEY", "YOUR SECRET");
+            var config = new AmazonDynamoDBConfig()
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.APSouth1
+            };
+            var client = new AmazonDynamoDBClient(credentials, config);
+            services.AddSingleton<IAmazonDynamoDB>(client);
+            services.AddSingleton<IDynamoDBContext, DynamoDBContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DynamoDbDemo v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
